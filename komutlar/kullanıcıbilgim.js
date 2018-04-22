e36df1db46aa53f653878f37f480d72b330162ef
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-22 22:44:59 +0300

    Update kullanıcıbilgim.js

--- komutlar/kullanıcıbilgim.js
+++ komutlar/kullanıcıbilgim.js
@@ -1,22 +1,25 @@
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message, params) => {
+exports.run = (client, message, args) => {
+  let guild = message.guild
+  let user = message.mentions.users.first();
+  if (message.mentions.users.size < 1) return message.reply('Kimin Kullanıcı Bilgisini İstediğini Yazmalısın').catch(console.error);
+  let mesaj = args.slice(0).join(' ');
     if (message.channel.type !== "group") {
-        var Durum = message.author.presence.status;
-        var Durm = (Durum == "online" ? (0x00AE86) : (Durum == "offline" ? (0x808080) : (Durum == "idle" ? (0xFFFF00) : (Durum == "dnd" ? (0xFF0000) : (0x00AE86)))))
-        var durm = (Durum == "online" ? ("Çevrimiçi") : (Durum == "offline" ? ("Çevrimdışı") : (Durum == "idle" ? ("Boşta") : (Durum == "dnd" ? ("Rahatsız Etmeyin") : ("Bilinmiyor/bulunamadı.")))))
+        var userDurum = user.presence.status;
+        var userDurm = (userDurum == "online" ? (0x00AE86) : (userDurum == "offline" ? (0x808080) : (userDurum == "idle" ? (0xFFFF00) : (userDurum == "dnd" ? (0xFF0000) : (0x00AE86)))))
+        var userdurm = (userDurum == "online" ? ("Çevrimiçi") : (userDurum == "offline" ? ("Çevrimdışı") : (userDurum == "idle" ? ("Boşta") : (userDurum == "dnd" ? ("Rahatsız Etmeyin") : ("Bilinmiyor/bulunamadı.")))))
       const kullanicibilgimk = new Discord.RichEmbed()
       .setAuthor(message.author.username, message.author.avatarURL)
       .setColor("RANDOM")
       .setTimestamp()
-      .addField('Ad:', message.author.username + '#' + message.author.discriminator)
-      .addField('ID:', message.author.id)
-      .addField('Kayıt tarihi:', message.author.createdAt)
-      .addField('Durum:', durm)
-      .addField('Şu an oynadığı oyun:', message.author.presence.game ? message.author.presence.game.name : 'Şu an oyun oynamıyor')
-      .addField('BOT mu?', message.author.bot ? '\n Evet' : 'Hayır')
-      console.log("!kullanıcıbilgim komutu " + message.author.username + " tarafından kullanıldı.")
+      .addField('Ad:', user.username + '#' + user.discriminator)
+      .addField('ID:', user.id)
+      .addField('Kayıt tarihi:', user.createdAt)
+      .addField('Durum:', userdurm)
+      .addField('Şu an oynadığı oyun:', user.presence.game ? user.presence.game.name : 'Şu an oyun oynamıyor')
+      .addField('BOT mu?', user.bot ? '\n Evet' : 'Hayır')
       return message.channel.sendEmbed(kullanicibilgimk);
   }
 };
@@ -24,12 +27,12 @@ exports.run = (client, message, params) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['kullanıcı', 'kullanıcı bilgim', 'kbilgim'],
+  aliases: ['kullanıcı', 'kullanıcı bilgim', 'kbilgim','kb'],
   permLevel: 0
 };
 
 exports.help = {
   name: 'kullanıcıbilgim',
-  description: 'Komutu kullanan kişi hakkında bilgi verir.',
+  description: 'Etiketlene kişinin hakkında bilgi verir.',
   usage: 'kullanıcıbilgim'
 };
