83e10422465a6b050e1843e10f5eced48e9dd7ac
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-22 18:27:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,19 +9,17 @@ require('./util/eventLoader')(client);
 var prefix = ayarlar.prefix;
 
 const log = message => {
-  if (!msg.content.startsWith(prefix)) {
-    console.log(`[${msg.author.tag}]  : ${msg.content}`);
-    return;
-  };
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
   if (err) console.error(err);
-  log(`${files.length} Komut Hazırlanacak.`);
+  log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
     let props = require(`./komutlar/${f}`);
-    log(`Hazır Komut  : ${props.help.name}.`);
+    log(`Yüklenen komut: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
@@ -79,17 +77,12 @@ client.unload = command => {
     }
   });
 };
+
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
     msg.reply('Aleyküm selam,  hoş geldin ^^');
   }
-  if (msg.content.toLowerCase() === 'r,çağır') {
-    msg.reply('**Çağrın Yetkililere İletildi Yakında Burdalar! Sen Merak Etme!**');
-    msg.react("✅")
-  }
 });
-
-
 
 client.elevation = message => {
   if(!message.guild) {
@@ -113,12 +106,5 @@ client.on('warn', e => {
 client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
-client.on("message", message => {
 
-  const kufur = ["amk", "aq","oç","şerefsiz","orruspu çocuğu",",sikik","sex","31 çekelim","aq","piç"];
-  if (kufur.some(word => message.content.includes(word)) ) {
-      message.reply("**Küfür Etme! Yasak** :rage:")
-      message.delete()
-  }
-});
 client.login(ayarlar.token);
