3f98af6e16f523d65df5f1e75e9121d6b785338a
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-22 13:22:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.unload = command => {
 
 client.on('message', msg => {
 	  if (!msg.content.startsWith(prefix)) {
-    console.log(`[${msg.guild.name}][${msg.author.tag}]  : ${msg.content}`);
+    console.log(`[${msg.author.tag}]  : ${msg.content}`);
     return;
   }
   if (msg.content.toLowerCase() === 'sa') {
