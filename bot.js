35225ef424c82223467353068e7c57a3610f2133
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-25 17:39:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,16 +12,16 @@ const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 client.on("guildCreate", guild => {
-    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 2 } });
+    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 3 } });
     client.channels.get("438704752633577482").sendMessage(`**--------------------------------------------------------------------------**\n__**SUNUCUYA KATILDIM**__\n\n   **Sunucu Adı**\n    ${guild.name}\n   **Sunucu ID**\n    ${guild.id}\n   **Sahibi**\n    ${guild.owner.user.tag}\n   **Sahip ID**\n    ${guild.owner.user.id}\n**--------------------------------------------------------------------------**`);
 });
 
 client.on("ready", guild => {
-    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 2 } });
+    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 3 } });
 });
 
 client.on("guildDelete", guild => {
-    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 2 } });
+    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 3 } });
     client.channels.get("438704752633577482").sendMessage(`**--------------------------------------------------------------------------**\n__**SUNUCUDAN AYRILDIM**__\n\n   **Sunucu Adı**\n    ${guild.name}\n   **Sunucu ID**\n    ${guild.id}\n   **Sahibi**\n    ${guild.owner.user.tag}\n   **Sahip ID**\n    ${guild.owner.user.id}\n**--------------------------------------------------------------------------**`);
 });
 client.commands = new Discord.Collection();
