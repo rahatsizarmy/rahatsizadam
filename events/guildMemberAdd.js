91c843fe2697ad5096d3b55473e1ebf3df0ae48d
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-25 19:01:14 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,4 @@
 module.exports = member => {
     let username = member.user.username;
-    member.send('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
+
 };
