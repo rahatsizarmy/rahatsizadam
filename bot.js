d7200d0db09802d7a2c623577bc1379d6153da83
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-25 17:29:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,19 @@ fs.readdir('./komutlar/', (err, files) => {
     });
   });
 });
+client.on("guildCreate", guild => {
+    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 1 } });
+    client.channels.get("438704752633577482").sendMessage(`**--------------------------------------------------------------------------**\n__**SUNUCUYA KATILDIM**__\n\n   **Sunucu Adı**\n    ${guild.name}\n   **Sunucu ID**\n    ${guild.id}\n   **Sahibi**\n    ${guild.owner.user.tag}\n   **Sahip ID**\n    ${guild.owner.user.id}\n**--------------------------------------------------------------------------**`);
+});
 
+client.on("ready", guild => {
+    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 1 } });
+});
+
+client.on("guildDelete", guild => {
+    client.user.setPresence({ game: { name: '' + prefix + 'yardım | ' + client.guilds.size + ' sunucu | V.2.0.1', type: 1 } });
+    client.channels.get("438704752633577482").sendMessage(`**--------------------------------------------------------------------------**\n__**SUNUCUDAN AYRILDIM**__\n\n   **Sunucu Adı**\n    ${guild.name}\n   **Sunucu ID**\n    ${guild.id}\n   **Sahibi**\n    ${guild.owner.user.tag}\n   **Sahip ID**\n    ${guild.owner.user.id}\n**--------------------------------------------------------------------------**`);
+});
 client.reload = command => {
   return new Promise((resolve, reject) => {
     try {
