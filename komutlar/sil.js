8308ed89354777de295d83fd581a12c6b75e96f3
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-29 14:36:10 +0300

    Update sil.js

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -4,20 +4,20 @@ exports.run = function(client, message, args) {
   if (!message.guild) {
     return message.author.send('`sil` komutu sadece sunucularda kullanılabilir.');
   }
-  let mesajsayisi = parseInt(args.join(' '));
-  if (mesajsayisi.length < 2) return message.channel.send('**1 ile 100 arası bir rakam yazmalısın**')
-  if (mesajsayisi > 100) return message.channel.send('**100 den fazla mesaj silemem!**')
-  if (mesajsayisi = 0) return message.channel.send('**1 ile 100 arası bir rakam yazmalısın**');
-  message.channel.bulkDelete(mesajsayisi + 1);
-  message.channel.send(mesajsayisi+" Adet Mesaj Siliyorum!")
+  let mesaj = args.slice(0).join(' ');
+  if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
+  if (mesaj > 100) return message.channel.send('**100 den fazla mesaj silemem!**')
+  if (mesaj = 0) return message.channel.send('**1 ile 100 arası bir rakam yazmalısın**');
+  message.channel.bulkDelete(mesaj + 1);
+  message.channel.send(mesaj+" Adet Mesaj Siliyorum!")
   .then(m => {
-        m.edit(mesajsayisi+ " Adet Meaj Sildim.!İyi Günler",`<@${message.author.id}>`);
+        m.edit(mesaj+ " Adet Meaj Sildim.!İyi Günler",`<@${message.author.id}>`);
       });
         const embed = new Discord.RichEmbed()
         .setColor("RANDOM")
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-  .addField('❯  Silinen Mesaj Sayısı',mesajsayisi)
+  .addField('❯  Silinen Mesaj Sayısı',mesaj)
   .addField('❯  Yetkili',`<@${message.author.id}>`)
   .addField('❯  Sonuç','Sonuç Başarılıdır')
 
@@ -29,12 +29,12 @@ exports.run = function(client, message, args) {
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['sil'],
+  aliases: ['temizle'],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'temizle',
+  name: 'sil',
   description: 'Belirlenen miktar mesajı siler.',
-  usage: 'temizle <temizlenecek mesaj sayısı>'
+  usage: 'sil <temizlenecek mesaj sayısı>'
 };
