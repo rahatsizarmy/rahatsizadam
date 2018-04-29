4a38f497bb5c50365cc826a614cb8ec2ba3440a1
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-29 22:11:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,11 +15,6 @@ client.on("guildCreate", guild => {
     client.channels.get("439843991014473739").sendMessage(`**--------------------------------------------------------------------------**\n__**SUNUCUYA KATILDIM**__\n\n   **Sunucu Adı**\n    ${guild.name}\n   **Sunucu ID**\n    ${guild.id}\n   **Sahibi**\n    ${guild.owner.user.tag}\n   **Sahip ID**\n    ${guild.owner.user.id}\n**--------------------------------------------------------------------------**`);
 });
 
-client.on("ready", guild => {
-  client.user.setPresence ({ game: { name: `${prefix}yardım | ${client.guilds.size} sunucu | V.2.0.1`, url: 'https://www.twitch.tv/batumurt1', type: 0 } });
-
-});
-
 client.on("guildDelete", guild => {
    client.channels.get("439843991014473739").sendMessage(`**--------------------------------------------------------------------------**\n__**SUNUCUDAN AYRILDIM**__\n\n   **Sunucu Adı**\n    ${guild.name}\n   **Sunucu ID**\n    ${guild.id}\n   **Sahibi**\n    ${guild.owner.user.tag}\n   **Sahip ID**\n    ${guild.owner.user.id}\n**--------------------------------------------------------------------------**`);
 });
