c822bc2b48b05dccedc38038615deca2b8b8b169
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-29 21:50:16 +0300

    Add files via upload

---
+++ komutlar/ayrıl.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+exports.run = (client, message, args) => {
+  if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`ayrıl` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  
+  message.guild.leave(message.guild.id);
+  client.channels.get("437551389342695426").sendMessage("-----------------------\n\nBaşarılı bir şekilde "+ message.guild.name +" Adlı sunucudan çıkarıldım.\n\n-----------------------")
+
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ['ayrıl'],
+    permLevel: 4
+  };
+  
+  exports.help = {
+    name: 'ayrıl',
+    description: 'Komutun kullanıldığı sunucudan botu çıkarır.',
+    usage: 'ayrıl'
+  };
+  
