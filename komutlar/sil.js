e89a2484f9c80238a85c051805dd5cd28ee24e0c
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-30 17:52:42 +0300

    Update sil.js

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -2,37 +2,25 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
 
   if (!message.guild) {
-    return message.author.send('`sil` komutu sadece sunucularda kullanılabilir.');
+    return message.author.send('`temizle` komutu sadece sunucularda kullanılabilir.');
   }
   let mesaj = parseInt(args.join(' '));
-  if (mesaj > 100) return message.channel.send('**100 den fazla mesaj silemem!**')
-  if (mesaj < 1) return message.channel.send('**1 ile 100 arası bir rakam yazmalısın**');
+  if (mesaj.length < mesaj) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin.')
+  if (mesaj > 100) return message.channel.send('100 adetden fazla mesaj silemem!');
   message.channel.bulkDelete(mesaj + 1);
-  message.channel.send(mesaj+" Adet Mesaj Siliyorum!")
-  .then(m => {
-        m.edit(mesaj+ " Adet Meaj Sildim.!İyi Günler",`<@${message.author.id}>`);
-      });
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-  .addField('❯  Silinen Mesaj Sayısı',mesaj)
-  .addField('❯  Yetkili',`<@${message.author.id}>`)
-  .addField('❯  Sonuç','Sonuç Başarılıdır')
-
-  return message.channel.sendEmbed(embed)
+  message.channel.send(mesaj +" adet mesaj sildim.Sildiren : `"+message.author.username+'#'+message.author.discriminator +'`')
+  message.channel.bulkDelete(2);
 };
-
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['temizle'],
+  aliases: ['sil'],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'sil',
+  name: 'temizle',
   description: 'Belirlenen miktar mesajı siler.',
-  usage: 'sil <temizlenecek mesaj sayısı>'
+  usage: 'temizle <temizlenecek mesaj sayısı>'
 };
