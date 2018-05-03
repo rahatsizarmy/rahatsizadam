84d830a1b1b9470d137f0dc1f4ee3103f85d73ba
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-03 17:11:12 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,9 +6,9 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Tüm komutlar başarıyla yüklendi StormBot`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setPresence ({ game: { name: `${prefix}yardım | ${client.guilds.size} sunucu | V.2.0.1`, url: 'https://www.twitch.tv/batumurt1', type: 0 } });
+  client.user.setGame(`${prefix}yardım ${client.guilds.size} Sunucu ${client.users.size} Kullanıcı`, "https://www.twitch.tv/hzsagkol");
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.memberCount + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
