3700f15786a202153f6322a224382c11554d88e3
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-04 18:49:04 +0300

    Update oylama.js

--- komutlar/oylama.js
+++ komutlar/oylama.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
     message.channel.send("@here")
 const embed = new Discord.RichEmbed()
 .setColor('RANDOM')
-    .setAuthor(message.author.username, message.author.avatarURL)
+    .setFooter(message.author.username, message.author.avatarURL)
 .setDescription(mesaj)
 .setAuthor(message.guild.name,message.guild.iconURL)
 message.channel.send(embed).then(msg => {
