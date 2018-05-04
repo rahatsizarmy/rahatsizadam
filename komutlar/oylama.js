1628043daf2ec44cf2a1a2c230ab5b872e80170d
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-04 18:39:00 +0300

    Create oylama.js

---
+++ komutlar/oylama.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+    let mesaj = args.slice(0).join(' ');
+    if (mesaj.length < 1) return message.reply('Yazmam İçin Birşey Yazmalısın!');
+    message.channel.send("@here")
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setFooter(message.guild.username, message.author.avatarURL)
+.setDescription(mesaj)
+.setAuthor(message.guild.username,message.guild.iconURL)
+message.channel.send(embed).then(msg => {
+
+  msg.react('✔').then(r => {
+    msg.react('❌')
+  })
+})
+};
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["anket","oylama"],
+    permLevel: 2
+  };
+  
+  exports.help = {
+    name: 'anketaç',
+    description: 'Anket Açar.',
+    usage: 'anket <anketiniz>'
+  };    
