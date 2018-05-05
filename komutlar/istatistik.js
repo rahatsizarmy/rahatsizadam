d1210eb5977a17659dd80b1f81d5f574695ca2f6
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-05 10:35:45 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -5,18 +5,9 @@ require("moment-duration-format");
 
 exports.run = (client, message) => {
   const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-const embed = new Discord.RichEmbed()
-.setColor("RANDOM")
-.setAuthor('İstatistikler',"https://cdn.discordapp.com/avatars/434771960849760258/b546921364b470cc10213d6c3e4ca6c3.png?size=2048")
-.setThumbnail("https://cdn.discordapp.com/avatars/434771960849760258/b546921364b470cc10213d6c3e4ca6c3.png?size=2048")
-.setFooter('© 2018 Rahatsız Adam|@ßatu Murt#7406 tarfından kodlandı.',"https://cdn.discordapp.com/avatars/434771960849760258/b546921364b470cc10213d6c3e4ca6c3.png?size=2048")
-.addField('❯  Açık kalma süresi: ',`${duration}`)
-.addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1')
-.addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB')
-.addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Pingim:  '+client.ping)
-return message.channel.sendEmbed(embed);
-
-};
+message.channel.send("İstatistikler hesaplanıyor...")
+.then(nmsg => nmsg.edit(new Discord.RichEmbed().addField("❯  İşletim istemi:","Linux").addField('❯  Açık kalma süresi: ',`${duration}`).addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1').addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB').addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Pingim:  '+client.ping).setColor('RANDOM').setAuthor('İstatistikler',"https://cdn.discordapp.com/avatars/434771960849760258/b546921364b470cc10213d6c3e4ca6c3.png?size=2048").setThumbnail("https://cdn.discordapp.com/avatars/434771960849760258/b546921364b470cc10213d6c3e4ca6c3.png?size=2048").setFooter('© 2018 Rahatsız Adam|@ßatu Murt#7406 tarfından kodlandı.',"https://cdn.discordapp.com/avatars/434771960849760258/b546921364b470cc10213d6c3e4ca6c3.png?size=2048").setTimestamp())
+)};
 exports.conf = {
   enabled: true,
   guildOnly: false,
