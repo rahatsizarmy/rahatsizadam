600fdc6784928faf19819facbdbbca82290e3fd8
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-06 22:36:49 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
   const channel = member.guild.channels.find('name', 'giriş-çıkış');
   if (!channel) return;
-  channel.send(`:inbox_tray: **|${member} sunucuya katıldı.`);
+  channel.send(`:inbox_tray: **|${member} sunucuya katıldı.**`);
 };
