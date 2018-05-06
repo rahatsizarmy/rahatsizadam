8ee6b5696fd51f7f937a7eb7800e139509b0f401
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-06 22:15:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setGame(`${prefix}yardım |${prefix}anket| ${client.guilds.size} sunucu ✨`, "https://www.twitch.tv/batumurt11");
+  client.user.setGame(`r,yardım |r,anket| ${client.guilds.size} sunucu ✨`, "https://www.twitch.tv/batumurt11");
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
