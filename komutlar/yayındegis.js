7ed1a8c0a92c6426a431db5eb79dbb2ceff50aa8
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-07 14:12:01 +0300

    Update yayındegis.js

--- komutlar/yayındegis.js
+++ komutlar/yayındegis.js
@@ -1,12 +1,16 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
+	  if (message.author.id === ayarlar.sahip){
 	let twitchtitle = args.slice(0).join(' ');
 	if (twitchtitle.length < 1) return message.reply('Yayın için herhangi bir başlık yazmalısın.');
   message.delete();
   client.user.setStatus("");
   client.user.setGame(`${twitchtitle}`, 'https://www.twitch.tv/batumurt11');
   message.channel.send(`:white_check_mark: Twitch basligi: **${twitchtitle}** olarak deyiştirildi.`);
+	      } else {
+        message.channel.send(':x: **Üzgünüm Ama Bu Komutu Şimdilik Kullanamazsınız!**');
+    }
 };
 
 exports.conf = {
