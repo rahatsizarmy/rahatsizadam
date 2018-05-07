ffaec07c5a658dc17248f6065c1e99119c8ce66f
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-07 14:11:22 +0300

    Update yayınmodugg.js

--- komutlar/yayınmodugg.js
+++ komutlar/yayınmodugg.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
+    if (message.author.id === ayarlar.sahip) {
   message.delete();
   client.user.setStatus("");
   client.user.setGame(`r,yardım |r,anket| ${client.guilds.size} sunucu ✨`, "https://www.twitch.tv/batumurt11");
   message.channel.send(`:white_check_mark: Botun yayın modu kapatıldı.`);
+      } else {
+        message.channel.send(':x: **Üzgünüm Ama Bu Komutu Şimdilik Kullanamazsınız!**');
+    }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['yayin', 'twitch-off', 'off'],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
