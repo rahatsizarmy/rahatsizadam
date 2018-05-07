e6feb51038bc2177d0c0bf71456772332e8236e8
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-07 15:21:03 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,5 +2,5 @@ module.exports = member => {
   let guild = member.guild;
     const channel = member.guild.channels.find('name', 'giriş-çıkış');
     if (!channel) return;
-    channel.send(`:outbox_tray: **|${member} sunucudan ayrıldı.`);
+    channel.send(`:outbox_tray: **|${member} sunucudan ayrıldı.**`);
 };
