1f98bd50ad2ab10d66aa644559a44b123c0eebe6
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-08 18:37:25 +0300

    Update yeni-kullanıcı.js

--- komutlar/yeni-kullanıcı.js
+++ komutlar/yeni-kullanıcı.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
       let users = client.users;
       let searchTerm = args[0];
-      if(!searchTerm) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Kullanıcı ara;').setDescription(message.author.tag + ', kullanım: r,kullanıcı-ara <mesaj>.').setFooter('TheRenk', client.user.avatarURL).setTimestamp());
+            if(!searchTerm) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Kullanıcı ara;').setDescription(message.author.tag + ', kullanım: r,üye-bul <üye ismi>.').setThumbnail(client.user.avatarURLL).setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp());
       let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
       message.channel.send(`Arama sonuçları;\n~》` + matches.map(u => u.tag || 'Yok').join(',\n~》 '));
         };
