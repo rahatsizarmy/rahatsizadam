d3e31e02ca9633265d006f6b87bda82f6ef976ee
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-09 20:20:08 +0300

    Update sunucubilgi.js

--- komutlar/sunucubilgi.js
+++ komutlar/sunucubilgi.js
@@ -13,12 +13,13 @@ exports.run = (client, message, params) => {
     if (message.channel.type !== 'dm') {
       const sunucubilgi = new Discord.RichEmbed()
       .setColor("RANDOM")
+    .setTimestamp()
     .setDescription(message.guild.name +' ('+ message.guild.id + ')')
     .setThumbnail(message.guild.iconURL)
     .setFooter(message.guild.name, message.guild.iconURL)
-    .addField('❯  Sunucunun Bilgileri:','\n•Bölge  :  `'+ message.guild.region +'`\n•Oluşturulma tarihi : `'+ message.guild.createdAt +'`\n•Rol Sayısı  : '+ message.guild.roles.size)
-    .addField('❯  Kişilerle İlgili:','•Kişi sayısı :  '+ message.guild.memberCount +'\n•Sahibi  :'+ message.guild.owner + ' (' + message.guild.ownerID + ')')
-    .addField('❯  Kanal Bilgileri :','•Kanal Sayısı  :  ' + message.guild.channels.size +'\n•AFK kanalı : '+ message.guild.afkChannel)
+    .addField('❯  Sunucunun Bilgileri:','•Doğrulama seviyesi : '+message.guild.verificationLevel+'\n•Bölge  :  `'+ message.guild.region +'\n•Oluşturulma tarihi : '+ message.guild.createdAt +' / '+message.guild.createdAt.toLocaleDateString() +`\n•Rol Sayısı  : `+ message.guild.roles.size +'\n•Varsayılan rol : '+ message.guild.defaultRole +'\n•Emojiler : ' +message.guild.emojis.map(e=>e.toString()).join(" | ") || 'Yok')
+    .addField('❯  Kişilerle İlgili:','•Kişi sayısı :  '+ message.guild.members.filter(member => member.user.bot).size+ ' bot / '+ message.guild.memberCount+' üye' +'\n•Sahibi  :'+ message.guild.owner+ ' (' + message.guild.ownerID + ')')
+    .addField('❯  Kanal Bilgileri :','•Kanal Sayısı  :  ' + message.guild.channels.size +'\n•AFK kanalı : '+ message.guild.afkChannel + '\n•AFK zaman aşımı  : '+message.guild.afkTimeout)
     return message.channel.sendEmbed(sunucubilgi);
     }
 };
