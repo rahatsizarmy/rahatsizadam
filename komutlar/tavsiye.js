1336a72c984408d4e8f00e64ca40e82b2556f7d2
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-09 16:51:24 +0300

    Update tavsiye.js

--- komutlar/tavsiye.js
+++ komutlar/tavsiye.js
@@ -2,15 +2,26 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message, args) => {
-let mesaj = args.slice(0).join(' ');
-if (mesaj.length < 1) return message.reply('Tavsiyeni Yazmalısın.');
-const embed = new Discord.RichEmbed()
-.setAuthor(message.author.username, message.author.avatarURL)
-.setDescription(message.author.username +' Tavsiyen İçin Sana Çok Tavsiye Ederiz\n\nGereksiz Kullanımın Cezası Vardır.')
-.setColor("RANDOM")
-client.channels.get("437698426596229164").sendCode('asciidoc', `= Tavsiyede Bulunan =\n\n${message.author.username}#${message.author.discriminator}\n\n= Tavsiyesi = \n\n ${mesaj}`,'<430723591223640064>')
-return message.channel.sendEmbed(embed)
-};
+    let type = args.slice(0).join(' ');
+        if (type.length < 1) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Tavsiye komutu').setDescription(message.author.tag + ', kullanım şekli: r,tavsiye <tavsiyeniz>.').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp());
+    const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTitle('Tavsiye;')
+    .setDescription(message.author.tag + ', tavsiyeniz başarıyla gönderildi.Teşekkürler!')
+    .setFooter('Rahatsız Adam', client.user.avatarURL)
+    .setTimestamp()
+    message.channel.send(embed);
+    const embed2 = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setDescription('***'+message.author.tag+'*** tavsiyede bulundu.')
+    .addField('Kullanıcı bilgisi:','Kullanıcı ismi:\n'+ message.author.tag +'\nKullanıcı id:\n'+message.author.id, true)
+    .addField('Kullanılan yer:', message.guild.name+' sunucusu\n**Ve**\n'+message.channel.name+' kanalında kullanıldı', true)
+    .setFooter('Rahatsız Adam', client.user.avatarURL)
+    .setThumbnail(message.author.avatarURL)
+    .addField('Tavsiye:', type, true)
+    .setTimestamp()
+    client.channels.get('410451498259185667').send(embed2);
+  };
 
 exports.conf = {
   enabled: true,
