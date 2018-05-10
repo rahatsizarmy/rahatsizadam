6ac173c0d096cc0904fdc0b703e3b1c1deeb4e64
batubabab <37729661+salamcibaba@users.noreply.github.com>
2018-05-10 22:45:40 +0300

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,6 +0,0 @@
-module.exports = member => {
-  let guild = member.guild;
-    const channel = member.guild.channels.find('name', 'giriş-çıkış');
-    if (!channel) return;
-    channel.send(`:outbox_tray: **|${member} sunucudan ayrıldı.**`);
-};
