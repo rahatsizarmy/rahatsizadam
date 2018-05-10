614de7a50eea549996a6054501f3a20dfeb91eec
batubabab <37729661+salamcibaba@users.noreply.github.com>
2018-05-10 19:47:50 +0300

    Update sunucubilgi.js

--- komutlar/sunucubilgi.js
+++ komutlar/sunucubilgi.js
@@ -17,7 +17,7 @@ exports.run = (client, message, params) => {
     .setDescription(message.guild.name +' ('+ message.guild.id + ')')
     .setThumbnail(message.guild.iconURL)
     .setFooter(message.guild.name, message.guild.iconURL)
-    .addField('❯  Sunucunun Bilgileri:','•Doğrulama seviyesi : '+message.guild.verificationLevel+'\n•Bölge  :  `'+ message.guild.region +'\n•Oluşturulma tarihi : '+ message.guild.createdAt +' / '+message.guild.createdAt.toLocaleDateString() +`\n•Rol Sayısı  : `+ message.guild.roles.size +'\n•Varsayılan rol : '+ message.guild.defaultRole +'\n•Emojiler : ' +message.guild.emojis.map(e=>e.toString()).join(" | ") || 'Yok')
+    .addField('❯  Sunucunun Bilgileri:','•Doğrulama seviyesi : '+message.guild.verificationLevel+'\n•Bölge  :  '+ message.guild.region +'\n•Oluşturulma tarihi : '+ message.guild.createdAt +' / '+message.guild.createdAt.toLocaleDateString() +`\n•Rol Sayısı  : `+ message.guild.roles.size +'\n•Varsayılan rol : '+ message.guild.defaultRole +'\n•Emojiler : ' +message.guild.emojis.map(e=>e.toString()).join(" | ") || 'Yok')
     .addField('❯  Kişilerle İlgili:','•Kişi sayısı :  '+ message.guild.members.filter(member => member.user.bot).size+ ' bot / '+ message.guild.memberCount+' üye' +'\n•Sahibi  :'+ message.guild.owner+ ' (' + message.guild.ownerID + ')')
     .addField('❯  Kanal Bilgileri :','•Kanal Sayısı  :  ' + message.guild.channels.size +'\n•AFK kanalı : '+ message.guild.afkChannel + '\n•AFK zaman aşımı  : '+message.guild.afkTimeout)
     return message.channel.sendEmbed(sunucubilgi);
