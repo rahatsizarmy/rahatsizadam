77315b38aaedce15dfdc7304a00c41a5d7b84546
batubabab <37729661+salamcibaba@users.noreply.github.com>
2018-05-10 20:33:09 +0300

    Update 1vs1.js

--- komutlar/1vs1.js
+++ komutlar/1vs1.js
@@ -5,10 +5,10 @@ exports.run = (client, message, args) => {
     if (!message.guild) {
       return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Eval;').setDescription(message.author.username + ', bu komutu direkt mesajda kullanamazsın.').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp()); }
     let user = message.mentions.users.first();
-    if (message.mentions.users.size < 3) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription(message.author.tag + ', kullanım: r,1vs1 <@kullanıcı> <@kullanıcı> .').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp());
+    if (message.mentions.users.size < 3) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription(message.author.tag + ', kullanım: r,3v3 <@kullanıcı> <@kullanıcı> <@kullanıcı> .').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp());
     var sans = ["10'a","1'e","20'ye","30'a"]
     var sonuc = sans[Math.floor((Math.random() * sans.length))];
-      message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaş başladı!').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp())
+      message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaş başladı!').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp())
       .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşılıyor Pat Küt.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
       .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşılıyor Pat Küp.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
       .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşılıyor Pat küt.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
@@ -25,7 +25,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: '1vs1',
+  name: '3v3',
   description: 'Seçtiğiniz 3 kişiyi savaştırırsınız.',
-  usage: 'r,1vs1 <@kullanıcı> <@kullanıcı> <@kullanıcı>'
+  usage: 'r,3v3 <@kullanıcı> <@kullanıcı> <@kullanıcı>'
 };
