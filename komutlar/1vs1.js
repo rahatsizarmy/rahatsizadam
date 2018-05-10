08b74d512ce8c4429b56b521dcf932b24d97bf0b
batubabab <37729661+salamcibaba@users.noreply.github.com>
2018-05-10 20:28:32 +0300

    Update 1vs1.js

--- komutlar/1vs1.js
+++ komutlar/1vs1.js
@@ -5,16 +5,16 @@ exports.run = (client, message, args) => {
     if (!message.guild) {
       return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Eval;').setDescription(message.author.username + ', bu komutu direkt mesajda kullanamazsın.').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp()); }
     let user = message.mentions.users.first();
-    if (message.mentions.users.size < 2) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('5v5;').setDescription(message.author.tag + ', kullanım: r,1vs1 <@kullanıcı> <@kullanıcı> .').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp());
+    if (message.mentions.users.size < 3) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription(message.author.tag + ', kullanım: r,1vs1 <@kullanıcı> <@kullanıcı> .').setFooter('Rahatsız Adam', client.user.avatarURL).setTimestamp());
     var sans = ["10'a","1'e","20'ye","30'a"]
     var sonuc = sans[Math.floor((Math.random() * sans.length))];
       message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaş başladı!').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp())
-      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaşılıyor Pat Küt.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
-      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaşılıyor Pat Küp.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
-      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaşılıyor Pat küt.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
-      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaş Sonuçlanıyor.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
-      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaş bitti!').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
-      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('1vs1').setDescription('Savaşın Galibi: **' + user.tag+'** Helal olsun sana Ne Vurdun Be Adımın Canını 100 den **'+ sonuc +'** Kadar Düşürdün Ve Adam Pes Etti.').setImage("https://media1.giphy.com/media/6fReqjdbFYXJu/giphy.gif").setFooter('Rahatsızların  Savaşı yapıldı.', client.user.avatarURL).setTimestamp()))
+      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşılıyor Pat Küt.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
+      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşılıyor Pat Küp.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
+      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşılıyor Pat küt.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
+      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaş Sonuçlanıyor.').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
+      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaş bitti!').setFooter('Rahatsızların  Savaşı yapılıyor.', client.user.avatarURL).setTimestamp()))
+      .then(nmsg => nmsg.edit(new Discord.RichEmbed().setColor('RANDOM').setTitle('3v3').setDescription('Savaşın Galibi: **' + user.tag+'** Helal olsun sana Ne Vurdun Be Adımın Canını 100 den **'+ sonuc +'** Kadar Düşürdün Ve Adam Pes Etti.').setImage("https://media1.giphy.com/media/6fReqjdbFYXJu/giphy.gif").setFooter('Rahatsızların  Savaşı yapıldı.', client.user.avatarURL).setTimestamp()))
         };
 
 exports.conf = {
@@ -26,6 +26,6 @@ exports.conf = {
 
 exports.help = {
   name: '1vs1',
-  description: 'Seçtiğiniz 2 kişiyi savaştırırsınız.',
-  usage: 'r,1vs1 <@kullanıcı> <@kullanıcı>'
+  description: 'Seçtiğiniz 3 kişiyi savaştırırsınız.',
+  usage: 'r,1vs1 <@kullanıcı> <@kullanıcı> <@kullanıcı>'
 };
